ee7c675200b86bed6653a9d8a9900a1e276f9b18
Henry <ngendahk@gmail.com>
2018-02-03 13:04:51 +0300

    Tutorial6 	A tutorial on recursive make

---
+++ tutorial6/app2/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+float add(const float x, const float y)
+{
+    return x + y;
+}
+
+
+int main(int argc, char* argv[])
+{
+    const float result = add(2.0, 3.0);
+    printf("result=%.2f\n", result);
+    return 0;
+}
